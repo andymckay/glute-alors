03fe0f34014d559c3e285f148dab3595f782611c
Andy McKay <74699+andymckay@users.noreply.github.com>
2026-09-06 16:37:41 -0700

    add in dotfiles

--- glute/settings.py
+++ glute/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +135,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+load_dotenv()
 INTERVALS_TOKEN = os.getenv("INTERVALS_TOKEN", None)
 if not INTERVALS_TOKEN:
     print("Warning: no intervals.icu API Key found.")
+
