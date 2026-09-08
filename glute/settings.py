6505413160aa83209ee7dfd0b577a83ddb28ce61
Andy McKay <74699+andymckay@users.noreply.github.com>
2026-09-08 10:46:08 -0700

    dj_database_url

--- glute/settings.py
+++ glute/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -23,7 +24,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-z#vdq369mheu9lxu%yl
 # Set DJANGO_DEBUG to any string to enable, set to empty to turn off.
 DEBUG = not not os.getenv("DJANGO_DEBUG", True)
 
-ALLOWED_HOSTS = ["127.0.0.1", "glute1.vps.webdock.cloud"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "glute1.vps.webdock.cloud"]
 
 
 # Application definition
@@ -74,12 +75,7 @@ WSGI_APPLICATION = "glute.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, conn_health_checks=True)}
 
 
 # Password validation
