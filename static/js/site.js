f96f418105f904db727ac0af1c98e145216c3c2f
Andy McKay <74699+andymckay@users.noreply.github.com>
2026-09-10 16:55:15 -0700

    add in dark theme

--- static/js/site.js
+++ static/js/site.js
@@ -20,6 +20,27 @@ window.addEventListener("load", (event) => {
             document.getElementById("timezone-alert").classList.remove("d-none");
         }
     }
+
+    function switchTheme(event) {
+        let html = document.getElementsByTagName("html")[0];
+        let theme = html.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
+        html.setAttribute("data-bs-theme", theme);
+        localStorage.setItem("theme", theme);
+        event.preventDefault();
+    }
+
+
+    for (let el of document.getElementsByClassName("theme-switch")) {
+        el.addEventListener("click", switchTheme);
+    }
+
+    let theme = localStorage.getItem("theme");
+    if (!theme) {
+        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+    if (theme) {
+        document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", theme);
+    }
 });
 
 function initHeartRateCharts() {
